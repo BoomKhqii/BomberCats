0f57b6db3be0ee002adfe185b8e4f53cbb5a83b7
Khæi <boom.raeiy@gmail.com>
2024-10-27 09:07:11 +0800

    Player Movement and Map

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float initialSpeed = 1f;
+    private float playerSpeed = 0;
+    public float stun = 1;
+    public bool isDead = false;
+    public bool isMoving = false;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSpeed = initialSpeed;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            //transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * playerSpeed);
+            rb.velocity += transform.right * Input.GetAxisRaw("Horizontal") * playerSpeed;
+            isMoving = true;
+        }
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            //transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * playerSpeed);
+            rb.velocity += transform.forward * Input.GetAxisRaw("Vertical") * playerSpeed;
+            isMoving = true;
+        }
+    }
+}
