f220f0a4363ca542c5aba4dee7f31aabd25714df
Khæi <boom.raeiy@gmail.com>
2025-03-10 18:47:31 +0800

    Multiplayer added, Better Movement & other packages added

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,16 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    [SerializeField]
+    private float playerSpeed = 4.5f;
+    [SerializeField]
+    private float gravityValue = -9.81f;
+
+    private Vector2 movementInput = Vector2.zero;
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+       movementInput = context.ReadValue<Vector2>();
+    }
+
+    void Update()
+    {
+
+        Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+    /*
     public float initialSpeed = 1f;
     private float playerSpeed = 0;
     public float stun = 1;
     public bool isDead = false;
     public bool isMoving = false;
     public int range = 3;
-
     private Rigidbody rb;
 
     void Start()
@@ -40,4 +73,5 @@ public class PlayerController : MonoBehaviour
         }
 
     }
+    */
 }
