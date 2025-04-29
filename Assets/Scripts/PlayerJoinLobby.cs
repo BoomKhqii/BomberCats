f32f68fed492dcb6d644f75b34bd58d3c93435bb
Khæi <boom.raeiy@gmail.com>
2025-04-29 15:01:42 +0800

    Fixed general scripts

---
+++ Assets/Scripts/PlayerJoinLobby.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerJoinLobby : MonoBehaviour
+{
+    public int playerIndex;
+    private bool isReady = false;
+    public int selectedCharacter;
+
+    // This method runs when the player joins the lobby
+    private void Start()
+    {
+        playerIndex = GetComponent<PlayerInput>().playerIndex;
+        LobbyManager.instance.OnPlayerJoined(GetComponent<PlayerInput>());
+    }
+
+    // This method is called when the ready button is pressed
+    public void OnReadyButton(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isReady = !isReady; // Toggle the readiness state
+            LobbyManager.instance.UpdateReadyState(); // Notify the lobby manager to check readiness
+        }
+    }
+
+    public bool IsReady()
+    {
+        return isReady;
+    }
+
+    public void SetSelectedCharacter(int characterID)
+    {
+        selectedCharacter = characterID;
+    }
+
+    public int GetSelectedCharacter()
+    {
+        return selectedCharacter;
+    }
+}
