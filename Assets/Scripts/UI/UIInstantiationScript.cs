665fd64b0a72b2ce92e9dde9426b9e1bfd0ffeae
Khæi <boom.raeiy@gmail.com>
2025-05-27 12:33:31 +0800

    Working UI spawner

--- Assets/Scripts/UI/UIInstantiationScript.cs
+++ Assets/Scripts/UI/UIInstantiationScript.cs
@@ -5,25 +5,32 @@ using UnityEngine;
 public class UIInstantiationScript : MonoBehaviour
 {
     public Transform positionUI;
+    private bool isOnce = true;
 
-    private void Start() { Destroy(gameObject, 5f); }
+    private void Start() 
+    { 
+        StartCoroutine(Delay()); // Enable the collider after 5 seconds
+        Destroy(gameObject, 10f); 
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(3f);
+        GetComponent<Collider>().enabled = true;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            // Instantiate the UI prefab at the player's position
-            GameObject uiPrefab = other.GetComponent<GeneralPlayerController>().UIGameObject;
+            GameObject uiPrefab = other.GetComponent<GeneralPlayerController>().UIGameObject; // Instantiate the UI prefab at the player's position
             if (uiPrefab != null)
             {
-                other.GetComponent<GeneralPlayerController>().UIComponents(positionUI);
-                //Instantiate(uiPrefab, positionUI.position, Quaternion.Euler(80, 0, 0));
-                Destroy(gameObject);
+                other.GetComponent<GeneralPlayerController>().UIComponents(positionUI, isOnce);
+                isOnce = false; // Ensure this only happens once
             }
             else
-            {
                 Debug.LogError("UI prefab not found!");
-            }
         }
     }
 }
